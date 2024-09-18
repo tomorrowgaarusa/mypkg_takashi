4e810b255b5229c2c0a83d70985a446fff4185c0
Takashi NODA <tomorrowgaarusa@gmail.com>
2024-09-18 16:51:46 +0200

    first commit

---
+++ R/mylog.R
@@ -0,0 +1,12 @@
+#' myLogFunction
+#'
+#' Description of what the function does.
+#'
+#' @param x Description of parameter x.
+#' @return Description of the return value.
+#' @examples
+#' my_function(5)  # This is a valid example
+#' my_function(c(1, 2, 3))  # Another example
+mylog <- function(x){
+  return(log(x))
+}
